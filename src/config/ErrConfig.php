73a0937de962785d5de7ae1bf656d6172c4b1ba8
dougw <dougwilbourne@gmail.com>
2022-12-21 10:47:42 -0500

    updated ErrConfig to implement isLibraryRegistered and registerLIbrary methods

--- src/config/ErrConfig.php
+++ src/config/ErrConfig.php
@@ -8,6 +8,9 @@ declare (strict_types=1);
 namespace pvc\config;
 
 
+
+
+use Exception;
 use pvc\interfaces\config\ErrConfigInterface;
 
 /**
@@ -15,19 +18,60 @@ use pvc\interfaces\config\ErrConfigInterface;
  */
 class ErrConfig implements ErrConfigInterface
 {
+	protected array $libraryCodes;
 
+	/**
+	 * registerLibrary
+	 * @param string $libraryName
+	 * @param int $libraryCode
+	 */
+	public function registerLibrary(string $libraryName, int $libraryCode): void
+	{
+		if ($libraryCode != ($this->libraryCodes[$libraryName] ?? null)) {
+			$format = "Error attempting to re-register exception %s library with a different library code.";
+			$msg = sprintf($format, $libraryName);
+			throw new Exception($msg);
+		}
+		$this->libraryCodes[$libraryName] = $libraryCode;
+	}
 
-	public static function createExceptionCode(string $libraryName, int $localCatalogCode):	int
+	/**
+	 * isLibraryRegistered
+	 * @param string $libraryName
+	 * @return bool
+	 */
+	public function isLibraryRegistered(string $libraryName): bool
+	{
+		return isset($this->libraryCodes[$libraryName]);
+	}
+
+	/**
+	 * createExceptionCode
+	 * @param string $libraryName
+	 * @param int $localCatalogCode
+	 * @return int
+	 * @throws Exception
+	 */
+	public function createExceptionCode(string $libraryName, int $localCatalogCode):	int
 	{
 		$globalPrefix = (string) self::getLibraryCode($libraryName);
 		return (int) ($globalPrefix . $localCatalogCode);
 	}
 
-	public static function getLibraryCode(string $libraryName): int
+	/**
+	 * getLibraryCode
+	 * @param string $libraryName
+	 * @return int
+	 * @throws Exception
+	 */
+	public function getLibraryCode(string $libraryName): int
 	{
-		$globalPrefixes = [
-			'pvc\err\stock' => 1000,
-		];
-		return $globalPrefixes[$libraryName] ?? 0;
+		if (!$this->isLibraryRegistered($libraryName)) {
+			$format = "Error attempting to get library code for unregistered library %s.";
+			$msg = sprintf($format, $libraryName);
+			throw new Exception($msg);
+		}
+		return $this->libraryCodes[$libraryName];
 	}
+
 }
